fcd43fd66fce7af3940c1a7c7f0c368a05757aa0
nayeoz <shizfukedup@gmail.com>
2026-06-06 14:55:18 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,14 @@
 import streamlit as st
 
+action = st.menu_button("Export", options=["CSV", "JSON", "PDF"])
+if action == "CSV":
+    st.write("Exporting as CSV...")
+elif action == "JSON":
+    st.write("Exporting as JSON...")
+elif action == "PDF":
+    st.write("Exporting as PDF...")
+import streamlit as st
+
 # Data Ar unsur
 data_ar = {
 "H": 1,
